819f48b50b1f304fa58287636c3e510af3995497
Bill Peterman <peterman.73@osu.edu>
2026-03-30 08:12:57 -0400

    update README

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# ResistanceGA
+# ResistanceGA2
 
-### An R package to optimize resistance surfaces using Genetic Algorithms. Both continuous and categorical surfaces can be optimized using these functions. Additionally, it is possible to simultaneously optimize multiple resistance surfaces at the same time to generate novel resistance surfaces. Resistance distances can be calculated as cost distances (least cost path) between points, or as circuit-based resistance distances calculated using CIRCUITSCAPE
+### An R package to optimize resistance surfaces using Genetic Algorithms. Both continuous and categorical surfaces can be optimized using these functions. Additionally, it is possible to simultaneously optimize multiple resistance surfaces at the same time to generate novel resistance surfaces. Resistance distances can be calculated as cost distances (least cost path) between points, or as circuit-based resistance distances calculated using CIRCUITSCAPE.
 
 To install this package, execute the following commands in R:
 
-    # Install 'devtools' package, if needed
-    if(!("devtools" %in% list.files(.libPaths()))) {
-        install.packages("devtools", repo = "http://cran.rstudio.com", dep = TRUE) 
-    } 
+```         
+# Install 'devtools' package, if needed
+if(!("devtools" %in% list.files(.libPaths()))) {
+    install.packages("devtools", repo = "http://cran.rstudio.com", dep = TRUE) 
+} 
 
-    devtools::install_github("wpeterman/ResistanceGA", build_vignettes = TRUE) # Download package
+devtools::install_github("wpeterman/ResistanceGA2", build_vignettes = TRUE) # Download package
 
-    library(ResistanceGA) # Loads package and the other dependnecies
-
-If installation with `build_vignettes = TRUE` fails, try installing the `tinytex` R package and then attempt to install `ResistanceGA` again.
+library(ResistanceGA2) # Loads package and the other dependnecies
+```
 
 Once the package is installed, you can view the 'Getting Started' vignette in R.
 
@@ -21,7 +21,9 @@ Once the package is installed, you can view the 'Getting Started' vignette in R.
 
 ### Other notes
 
-Optimization with CIRCUITSCAPE (v4) is still possible, although not actively supported in the most recent version. If you wish to optimize using CIRCUITSCAPE, it is highly recommended that you install Julia and the CIRCUITSCAPE Julia package. General instructions [**here**](https://petermanresearch.weebly.com/uploads/2/5/9/2/25926970/julia_guide.pdf "Julia Guide"). There is also a `Julia_Guide` vignette with the package now.
+-   This is large and ongoing update. `terra` is now fully (and only) supported. There is also a switch away from `JuliaCall` to `JuliaConnectoR`. Please make me aware of issues / errors as you encounter them.
+
+-   It is highly recommended that you install Julia and the CIRCUITSCAPE Julia package. General instructions [**here**](https://petermanresearch.weebly.com/uploads/2/5/9/2/25926970/julia_guide.pdf "Julia Guide"). There is also a `Julia_Guide` vignette with the package now.
 
 ------------------------------------------------------------------------
 
@@ -29,7 +31,6 @@ This approach has been developed from the methods first utilized in Peterman et 
 
 Peterman, W.E., G.M. Connette, R.D. Semlitsch, and L.S. Eggert. 2014. Ecological resistance surfaces predict fine-scale genetic differentiation in a terrestrial woodland salamander. Molecular Ecology 23:2402–2413. [**PDF**](http://petermanresearch.weebly.com/uploads/2/5/9/2/25926970/peterman_et_al._2014--mec.pdf "Peterman et al.")
 
-Peterman, W. E. 2018. ResistanceGA: An R package for the optimization of resistance surfaces using genetic algorithms. Methods in Ecology and Evolution 9, 1638–1647.
-<doi:10.1111/2041-210X.12984>. [**PDF**](https://besjournals.onlinelibrary.wiley.com/doi/abs/10.1111/2041-210X.12984 "MEE Publication")
+Peterman, W. E. 2018. ResistanceGA: An R package for the optimization of resistance surfaces using genetic algorithms. Methods in Ecology and Evolution 9, 1638–1647. <doi:10.1111/2041-210X.12984>. [**PDF**](https://besjournals.onlinelibrary.wiley.com/doi/abs/10.1111/2041-210X.12984 "MEE Publication")
 
 Ruiz-López, M.J., Barelli, C., Rovero, F., Hodges, K., Roos, C., Peterman, W.E., Ting, N., 2016. A novel landscape genetic approach demonstrates the effects of human disturbance on the Udzungwa red colobus monkey (*Procolobus gordonorum*). Heredity, Ruiz-López 116, 167–176. <https://doi.org/10.1038/hdy.2015.82>
---
+++ vignettes/GettingStarted.R
@@ -0,0 +1,150 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  devtools::install_github("wpeterman/ResistanceGA", build_vignettes = TRUE)
+
+## ----fig.width=6, fig.asp=0.65------------------------------------------------
+library(ResistanceGA2)
+
+plot(raster_orig)
+
+plot(raster_true)
+
+plot(raster_true$multi_true, main = 'Multivariate Resistance')
+plot(sample_pops$sample_multi, add = T, pch = 19, col = 'blue')
+
+par(mfrow = c(2,1))
+plot(lower(Dc_list$Dc_multi) ~
+       c(dist(sample_pops$sample_multi@coords)),
+     xlab = 'Euclidean distance', ylab = 'Chord distance')
+
+plot(lower(Dc_list$Dc_multi) ~
+       lower(resist_list$resist_multi),
+     xlab = 'Resistance distance', ylab = 'Chord distance')
+par(mfrow = c(1,1))
+
+
+## ----eval = F-----------------------------------------------------------------
+#  GA.prep(ASCII.dir,            # REQUIRED
+#          Results.dir = NULL,   # REQUIRED
+#          min.cat = NULL,
+#          max.cat = 2500,
+#          max.cont = 2500,
+#          min.scale = NULL,
+#          max.scale = NULL,
+#          cont.shape = NULL,
+#          select.trans = 'M',
+#          method = "LL",
+#          scale = FALSE,
+#          scale.surfaces = NULL,
+#          k.value = 2,
+#          pop.mult = 15,
+#          percent.elite = 0.05,
+#          type = "real-valued",
+#          pcrossover = 0.85,
+#          pmutation = 0.125,
+#          maxiter = 1000,
+#          run = NULL,
+#          keepBest = TRUE,
+#          population = gaControl(type)$population,
+#          selection = gaControl(type)$selection,
+#          crossover = "gareal_blxCrossover",
+#          mutation = gaControl(type)$mutation,
+#          pop.size = NULL,
+#          parallel = FALSE,
+#          gaisl = FALSE,
+#          island.pop = 20,
+#          numIslands = NULL,
+#          migrationRate = NULL,
+#          migrationInterval = NULL,
+#          optim = FALSE,
+#          optim.method = "L-BFGS-B",
+#          poptim = 0.0,
+#          pressel = 1.00,
+#          control = list(fnscale = -1, maxit = 100),
+#          hessian = FALSE,
+#          opt.digits = NULL,
+#          seed = NULL,
+#          monitor = TRUE,
+#          quiet = FALSE)
+
+## ----eval = FALSE-------------------------------------------------------------
+#  gdist.prep(n.Pops,           # REQUIRED
+#             response = NULL,  # REQUIRED
+#             samples,          # REQUIRED
+#             covariates = NULL,
+#             formula = NULL,
+#             transitionFunction = function(x)  1 / mean(x),
+#             directions = 8,
+#             longlat = FALSE,
+#             method = 'commuteDistance',
+#             min.max_dist = NULL,
+#             keep = NULL)
+
+## ----eval = FALSE-------------------------------------------------------------
+#  jl.prep(n.Pops,             # REQUIRED
+#          response = NULL,    # REQUIRED
+#          CS_Point.File,      # REQUIRED
+#          covariates = NULL,
+#          formula = NULL,
+#          JULIA_HOME = NULL,  # REQUIRED
+#          Neighbor.Connect = 8,
+#          pairs_to_include = NULL,
+#          pop2ind = NULL,
+#          nb = NULL,
+#          parallel = FALSE,
+#          cores = NULL,
+#          cholmod = TRUE,
+#          precision = FALSE,
+#          run_test = TRUE,
+#          write.files = NULL,
+#          write.criteria = NULL,
+#          silent = TRUE,
+#          Julia_link = 'JuliaCall',
+#          scratch = NULL,
+#          rm.files = TRUE)
+
+## ----eval = FALSE-------------------------------------------------------------
+#  Plot.trans(PARM,
+#             Resistance,
+#             transformation,
+#             scale,
+#             print.dir,
+#             marginal.plot,
+#             marg.type,
+#             Name)
+
+## ----fig.width=6, fig.asp=0.65------------------------------------------------
+(Plot.trans(PARM = c(14.5, 100), 
+            Resistance = raster_orig$cont_orig, 
+            transformation = 1))
+
+## This creates the same plot
+# (Plot.trans(PARM = c(2.5, 100), 
+#             Resistance = raster_orig$cont_orig, 
+#             transformation = "Reverse Monomolecular"))
+
+(Plot.trans(PARM = c(2.5, 100), 
+            Resistance = c(1,10), 
+            transformation = 1))
+
+## ----eval=F-------------------------------------------------------------------
+#  gdist.inputs <- gdist.prep(n.Pops = length(sample_pops$sample_cont),
+#                             samples = sample_pops$sample_cont)
+#  
+#  gdist.out <- Run_gdistance(gdist.inputs = gdist.inputs,
+#                             r = raster_true$cont_true)
+
+## ---- echo=F, eval=T----------------------------------------------------------
+try(jl.inputs <- jl.prep(n.Pops = length(sample_pops$sample_cont),
+                         CS_Point.File = sample_pops$sample_cont,
+                     JULIA_HOME = "C:/Users/peterman.73/AppData/Local/Programs/Julia-1.6.1/bin/"), silent = T)
+
+## ----eval=F-------------------------------------------------------------------
+#  jl.inputs <- jl.prep(n.Pops = length(sample_pops$sample_cont),
+#                       CS_Point.File = sample_pops$sample_cont,
+#                       JULIA_HOME = "C:/Users/peterman.73/AppData/Local/Programs/Julia-1.6.1/bin/")
+#  jl.out <- Run_CS.jl(jl.inputs = jl.inputs,
+#                      r = raster_true$cont_true)
+
